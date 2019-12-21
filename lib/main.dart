de758003ae046993541ad079c791d87fb3144c00
robinsontom <robinsontom@google.com>
2019-12-20 16:24:53 -0800

    Add jump control

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,47 @@ import 'package:flutter_widgets/flutter_widgets.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  ItemScrollController controller = ItemScrollController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Material(
-        child: PageView(
-          children: <Widget>[
-            KeyedSubtree(
-              key: PageStorageKey('page1'),
-              child: ScrollablePositionedList.builder(
-                itemCount: 1000,
-                itemBuilder: (context, index) => Text('Item $index'),
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                children: <Widget>[
+                  KeyedSubtree(
+                    key: PageStorageKey('page1'),
+                    child: ScrollablePositionedList.builder(
+                      itemCount: 1000,
+                      itemBuilder: (context, index) => Text('Item $index'),
+                      itemScrollController: controller,
+                    ),
+                  ),
+                  Text('house'),
+                  KeyedSubtree(
+                    key: PageStorageKey('page3'),
+                    child: ScrollablePositionedList.builder(
+                      itemCount: 1000,
+                      itemBuilder: (context, index) => Text('Item $index'),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text('house'),
-            KeyedSubtree(
-              key: PageStorageKey('page3'),
-              child: ScrollablePositionedList.builder(
-                itemCount: 1000,
-                itemBuilder: (context, index) => Text('Item $index'),
+            GestureDetector(
+              onTap: () {
+                print('Jumping');
+                controller.jumpTo(index: 100);
+              },
+              child: Text(
+                'Jump',
+                style: TextStyle(fontSize: 20),
               ),
-            ),
+            )
           ],
         ),
       ),
